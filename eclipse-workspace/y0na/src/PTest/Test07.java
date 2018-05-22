36336a36044da7a0dd789f8fec4fe47117834991
y0na <gusrl0515@gmail.com>
2018-05-22 21:43:39 +0900

    yona

--- eclipse-workspace/y0na/src/PTest/Test07.java
+++ eclipse-workspace/y0na/src/PTest/Test07.java
@@ -2,25 +2,18 @@ package PTest;
 
 public class Test07 {
 	public static void main(String args[]){
-		Data data=new Data();
-		Data02 data02=new Data02();
+		int a=5;
+		Node node=new Node(a);
 		 
 
 	}
 }
 
-class Data{
-	int a;
-}
-
-class Data02{
-	int a;
-	
-	Data02(){
-	 
-	}
-	
-	Data02(int x){
-		x=a;
-	}
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
 }
