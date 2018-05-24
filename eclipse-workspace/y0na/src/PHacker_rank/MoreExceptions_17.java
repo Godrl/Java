4bf84c4cb7c68507b61a2874c1ce488872c147cb
y0na <gusrl0515@gmail.com>
2018-05-24 13:02:43 +0900

    yona

---
+++ eclipse-workspace/y0na/src/PHacker_rank/MoreExceptions_17.java
@@ -0,0 +1,40 @@
+package PHacker_rank;
+import java.util.*;
+import java.io.*;
+
+public class MoreExceptions_17 {
+	public static void main(String[] args) {		    
+		Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+		        int n = in.nextInt();
+		        int p = in.nextInt();
+		        Calculator myCalculator = new Calculator();
+		        try {
+		        	int ans = myCalculator.power(n, p);
+		        	System.out.println(ans);
+		        }
+		        catch (Exception e) {
+		        	System.out.println(e.getMessage());
+		        }
+        }
+        in.close();
+	}
+}
+	
+class Calculator{
+	Calculator(){
+		
+	}
+	 
+	int power(int a, int b) throws Exception{
+		if(a >= 0 && b >= 0) {
+			return (int)Math.pow(a, b);
+		}
+		else {
+			throw new Exception("n and p should be non-negative");
+		}
+		
+	}
+	
+}
